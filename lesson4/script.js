291569521e182791b5233f1769ebc39e34a65b17
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2024-07-24 20:32:29 +0300

    Homework4

--- lesson4/script.js
+++ lesson4/script.js
@@ -20,3 +20,50 @@ button.addEventListener('click', () => {
 // 3. Необходимо при отправке формы проверить, заполнены ли все поля в этой форме. Если какое-либо поле не заполнено, форма не должна отправляться, также должны быть подсвечены незаполненные поля (необходимо поставить класс error незаполненным полям). Как только пользователь начинает заполнять какое-либо поле, необходимо, при вводе в данное поле, произвести проверку:
 // - Если поле пустое, необходимо данное поле подсветить (поставить класс error данному полю).
 // - Если поле было чем-либо заполнено, подсветку (класс error) необходимо убрать.
+const form = document.querySelector('form');
+const input2 = document.querySelector('.form-control');
+const select = document.querySelector('select');
+
+form.addEventListener('submit', (e) => {
+  let valid = true;
+
+  if (input2.value.trim() === '') {
+    input2.classList.add('error');
+    valid = false;
+  } else {
+    input2.classList.remove('error');
+    input2.classList.add('valid');
+  }
+
+  if (select.value.trim() === '') {
+    select.classList.add('error');
+    valid = false;
+  } else {
+    select.classList.remove('error');
+    select.classList.add('valid');
+  }
+
+  if (!valid) {
+    e.preventDefault();
+  }
+});
+
+// в режиме реального времени валидация input
+input2.addEventListener('input', () => {
+  if (input2.value.trim() === '') {
+    input2.classList.add('error');
+  } else {
+    input2.classList.remove('error');
+    input2.classList.add('valid');
+  }
+});
+
+// в режиме реального времени валидация select
+select.addEventListener('change', () => {
+  if (select.value.trim() === '') {
+    select.classList.add('error');
+  } else {
+    select.classList.remove('error');
+    select.classList.add('valid');
+  }
+});
